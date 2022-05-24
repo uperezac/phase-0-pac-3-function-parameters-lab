2f9d1554bf1a45d4c5df3fc904ddc0cd2af3d30b
Ulises Perez Acosta <u.perezacosta@gmail.com>
2022-05-23 20:24:10 -0400

    commit tree 6741ca372d145d3125d4a6dd490cd684abe34b7e

---
+++ index.js
@@ -0,0 +1,8 @@
+/*function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Ulises"))*/
+function introduction(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introduction("Ulises", JavaScript))
